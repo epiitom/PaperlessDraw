14f11dad4de59e2d7576daf7810f005d6fd122e5
Prathmesh Kale <PRATHMESHKALE0018@GMAIL.COM>
2025-06-24 15:38:32 +0530

    canvas shapes

---
+++ apps/excelidraw-frontend/draw/index.ts
@@ -0,0 +1,36 @@
+export function initDraw(canvas: HTMLCanvasElement){
+        const ctx = canvas.getContext("2d");
+
+           if(!ctx){
+            return
+           }
+      ctx.fillStyle= "rgba(0,0,0)";
+      ctx.fillRect(0,0,canvas.width,canvas.height);
+            
+
+         let clicked = false;
+         let startX = 0;
+         let startY = 0;
+
+         canvas.addEventListener("mousedown", (e) => {
+            clicked = true
+            startX = e.offsetX
+            startY = e.offsetY
+         })
+         canvas.addEventListener("mouseup" , (e) => {
+            clicked = false
+        
+         })
+         canvas.addEventListener("mousemove", (e) => {
+            if(clicked){
+                const width = e.offsetX-startX;
+                const height = e.offsetY - startY;
+                ctx.fillStyle = "black";
+          ctx.fillRect(0, 0, canvas.width, canvas.height);
+                ctx.strokeStyle ="rgba(255,255,255)"
+                ctx.strokeRect(startX,startY,width,height)
+                 
+            } 
+         })
+           ctx.strokeRect(25,25,100,100)
+    }
